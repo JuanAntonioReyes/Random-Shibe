bf3c9b76835f576082e01bdc99cdd25506dcdc84
JuanAntonioReyes <jantonio.reyes.navarro@gmail.com>
2018-05-08 15:23:43 +0200

    AJAX request to shibe.online

--- js/app.js
+++ js/app.js
@@ -4,5 +4,25 @@ $(document).ready(function() {
 
 	// Get new shibe button click event
 	var btnGetNew = $("#btnGetNew");
+	btnGetNew.click(getNewShibe);
+
+	function getNewShibe(e){
+		// Make the ajax request
+		$.ajax({
+			url: 'http://shibe.online/api/shibes?count=1',
+			type: 'GET',
+			dataType: 'JSON',
+			success: handleCheckSuccess,
+			error: handleError
+		})
+	}
+
+	function handleCheckSuccess(response) {
+		console.log(response);
+	}
+
+	function handleError(response) {
+		console.log("SOMETHING WENT WRONG - ERROR");
+	}
 
 })
